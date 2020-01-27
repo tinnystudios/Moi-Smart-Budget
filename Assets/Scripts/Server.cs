747d36b3f3729f3f1612b61b7e5ceaf0e4028f72
Tin <tinnystudios@gmail.com>
2020-01-28 06:27:14 +1100

    Refactor folders

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    // Post an expense
+    public IEnumerator PostExpense()
+    {
+        var url = "http://192.168.1.85/AddExpenses.php";
+        var formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
+        var www = UnityWebRequest.Post(url, formData);
+        www.chunkedTransfer = false;////ADD THIS LINE
+        yield return www.SendWebRequest();
+    }
+}
