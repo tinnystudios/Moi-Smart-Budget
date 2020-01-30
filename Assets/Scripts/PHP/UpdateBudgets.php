0d592c9a2dae98e5730ae4e331587aae740d6bd9
Tin <tin@liminalvr.com>
2020-01-30 14:37:43 +1100

    Fix update budget not setting name

--- Assets/Scripts/PHP/UpdateBudgets.php
+++ Assets/Scripts/PHP/UpdateBudgets.php
@@ -26,7 +26,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE budgets SET Amount='".$Amount."', StartDate='".$StartDate."', EndDate='".$EndDate."', RepeatType='".$RepeatType."'  WHERE id='".$Id."'";
+$sql = "UPDATE budgets SET Name='".$Name."', Amount='".$Amount."', StartDate='".$StartDate."', EndDate='".$EndDate."', RepeatType='".$RepeatType."'  WHERE id='".$Id."'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Successfully updated budget";
